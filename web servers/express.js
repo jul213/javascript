299ad300da8503bb400f4d17e9d4f24b2321c72b
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-15 22:44:28 +0000

    escucha del servidor express puerto  8000

--- web servers/express.js
+++ web servers/express.js
@@ -4,3 +4,7 @@ let app = express()
 app.get("/", function(request, response){
     response.send("hola desde express");
 })
+
+app.listen(8000, function(){
+    console.log("servidor express escuchando desde el puerto 8000");
+})
