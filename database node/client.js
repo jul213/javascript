30ab00086609ee5d03dd0bd64bbe47262348b259
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-16 15:24:34 +0000

    creacion de la conexion a y ejecucion de la consulta

--- database node/client.js
+++ database node/client.js
@@ -1,2 +1,20 @@
 const pg = require('pg')
 
+
+
+const client = new pg.Client(constring);
+
+client.connect((err) => {
+    if (!err){
+        client.query(
+            'select * from pokemon',
+            (err, result) =>{
+                if (err){
+                    console.log(err);
+                } else{
+                    console.log(result.rows);
+                }
+            }
+        )
+    }
+})
